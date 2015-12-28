76645a1686b16dcb0250ee722cc5f15bccf42931
Keijiro Takahashi <keijiro@ZX.local>
2015-12-28 14:22:22 +0900

    Add tester script.

---
+++ Assets/Test/Tester.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+public class Tester : MonoBehaviour
+{
+    void Update()
+    {
+        var lr = GetComponent<Spektr.LightningRenderer>();
+        lr.throttle = Input.GetMouseButton(0) ? 0.4f : 0.045f;
+    }
+
+    void OnGUI()
+    {
+        GUI.Label(new Rect(0, 0, 200, 200), "Press mouse button to burst.");
+    }
+}
